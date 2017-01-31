5d05a22d9d4f572b6bb46b69e22a15ab933c8e8d
Elias Munoz <eliasmuol@gmail.com>
2017-02-01 00:59:07 +0100

    Developed Main

--- src/com/eliasmuol/Main.java
+++ src/com/eliasmuol/Main.java
@@ -1,8 +1,11 @@
 package com.eliasmuol;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        // aqui esta direccion se guardaran los archivos de texto
+        //new Runner("C:\\Users\\usuario\\IdeaProjects\\agenda\\agenda.txt").start();
     }
 }
